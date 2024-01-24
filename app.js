f266853c65f93216163f0cdb05cc3427996faab2
Kiran P S <kiranps2607@gmail.com>
2024-01-25 00:10:11 +0530

    added layout

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 let boxes=document.querySelectorAll(".box");
 let resetbtn=document.querySelector(".resetbtn");
-console.log(boxes);
+let xturn=true;
+const winingpatt=[
+    [0,3,6],
+    [0,1,2],
+    [0,4,6],
+    [1,4,7],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+];
+boxes.forEach((box)=> {
+   box.addEventListener("click",()=> {
+    if (xturn) {
+         box.innerText="X";
+        
+    }
+    else {
+        box.innerText="O";
+        
+    }
+    xturn=!xturn;
+    box.disabled=true;
+   })
+})
